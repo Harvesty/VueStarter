97ac3efcf8a136395dde52d05488147a480c68aa
Harvesty <sohoearny@gmail.com>
2020-07-20 05:49:54 +0800

    添加项目文件。

---
+++ VueStarter/ClientApp/README.md
@@ -0,0 +1,29 @@
+# VueStarter
+
+## Project setup
+
+```cmd
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```cmd
+npm serve
+```
+
+### Compiles and minifies for production
+
+```cmd
+npm build
+```
+
+### Lints and fixes files
+
+```cmd
+npm lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ VueStarter/ClientApp/src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>{{info}}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'About',
+  data() {
+    return {
+      info: {}
+    };
+  },
+  mounted() {
+    axios
+      .get("api/weatherforecast")
+      .then(response => (this.info = response.data));
+  }
+}
+</script>
